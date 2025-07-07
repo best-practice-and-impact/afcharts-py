13b03947185f621feb06da31f0740feff3fa80a7
rodriguesfred <fred.rodrigues@defra.gov.uk>
2025-07-07 12:48:21 +0100

    updating test files to include function

--- tests/plotly_line_test.py
+++ tests/plotly_line_test.py
@@ -9,58 +9,61 @@ from afcharts.assets.af_colours import duo
 # AF Package
 from afcharts.theme_af import theme_af
 
-# Only set the renderer to "browser" if not running in CI
-if not os.environ.get("CI"):
-    pio.renderers.default = "browser"
 
-# Load the gapminder dataset from plotly.express
-df = px.data.gapminder()
+def test_plolty_line_chart():
+    # Only set the renderer to "browser" if not running in CI
+    if not os.environ.get("CI"):
+        pio.renderers.default = "browser"
 
-df = df[df["country"].isin(["United Kingdom", "China"])]
+    # Load the gapminder dataset from plotly.express
+    df = px.data.gapminder()
 
-fig = go.Figure()
+    df = df[df["country"].isin(["United Kingdom", "China"])]
 
-# Add a trace for each continent
-for country in df["country"].unique():
-    df_country = df[df["country"] == country]
-    fig.add_trace(
-        go.Scatter(
-            x=df_country["year"],
-            y=df_country["lifeExp"],
-            mode="lines",
-            name=country,
-            text=df_country["country"],
+    fig = go.Figure()
+
+    # Add a trace for each continent
+    for country in df["country"].unique():
+        df_country = df[df["country"] == country]
+        fig.add_trace(
+            go.Scatter(
+                x=df_country["year"],
+                y=df_country["lifeExp"],
+                mode="lines",
+                name=country,
+                text=df_country["country"],
+            )
         )
+
+    # Update layout
+    fig.update_layout(
+        template=theme_af(grid="y", colour_palette=duo),
+        xaxis=dict(
+            title="Year",
+        ),
+        title=dict(
+            text="Living Longer",
+            subtitle=dict(text="Life Expectancy in the United Kingdom and China 1952-2007"),
+        ),
+        margin=dict(b=70),  # Increase bottom margin to make space for source
+        annotations=[
+            dict(
+                text="Source: Gapminder",
+                xref="paper",
+                yref="paper",
+                x=0,
+                y=-0.1,
+                xanchor="left",
+            )
+        ],
+        meta=dict(
+            alt="Bar chart showing sales of Product A (23 units), Product B (45 units), and Product C (56 units)."
+        ),
     )
 
-# Update layout
-fig.update_layout(
-    template=theme_af(grid="y", colour_palette=duo),
-    xaxis=dict(
-        title="Year",
-    ),
-    title=dict(
-        text="Living Longer",
-        subtitle=dict(text="Life Expectancy in the United Kingdom and China 1952-2007"),
-    ),
-    margin=dict(b=70),  # Increase bottom margin to make space for source
-    annotations=[
-        dict(
-            text="Source: Gapminder",
-            xref="paper",
-            yref="paper",
-            x=0,
-            y=-0.1,
-            xanchor="left",
-        )
-    ],
-    meta=dict(alt="Bar chart showing sales of Product A (23 units), Product B (45 units), and Product C (56 units)."),
-)
+    fig.update_yaxes(range=[0, 82])
 
-fig.update_yaxes(range=[0, 82])
+    if not os.environ.get("CI"):
+        fig.show()
 
-if not os.environ.get("CI"):
-    fig.show()
-else:
-    if isinstance(fig, go.Figure):
-        pass
+    assert isinstance(fig, go.Figure)
